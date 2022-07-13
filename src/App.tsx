897a2a01ca943523f93ad21f706e72e8ddea30c3
Esa Niemi <esa.niemi@protonmail.com>
2022-07-13 23:35:22 +0300

    Set up barebone tooling

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const style: React.CSSProperties = {
+    backgroundColor: 'pink'
+}
+
+const App = () => {
+    return <div style={style}>asd</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+    },
+};
